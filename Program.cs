c21c48cc0096c44f240db59e4e4670e67fdd1910
Andrew Omondi <andrew.omondi@microsoft.com>
2021-10-05 15:52:34 +0300

    Proejct updates

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Microsoft.Kiota.Http.HttpClient;
+using Microsoft.Kiota.Http.HttpClientLibrary;
 using ApiSdk;
 using Azure.Identity;
 using Microsoft.Kiota.Authentication.Azure;
@@ -17,8 +17,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 ClientId = "INSERT_CLIENT_ID"
             };
             var interactiveBrowserCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
-            var httpCore = new HttpCore(new AzureIdentityAuthenticationProvider(interactiveBrowserCredential, scopes));
-            var graphClient = new GraphClient(httpCore);
+            var httpClientRequestAdapter = new HttpClientRequestAdapter(new AzureIdentityAuthenticationProvider(interactiveBrowserCredential, scopes));
+            var graphClient = new GraphClient(httpClientRequestAdapter);
             var user = await graphClient.Me.GetAsync();
 
             Console.WriteLine(user.DisplayName);
