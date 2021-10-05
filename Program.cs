22171398df89b3e46d839cc58624e76dda835de9
Andrew Omondi <andrew.omondi@microsoft.com>
2021-10-05 10:22:46 +0300

    Working test app

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Kiota.Http.HttpClient;
+using ApiSdk;
+using Azure.Identity;
+using Microsoft.Kiota.Authentication.Azure;
+using System.Threading.Tasks;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] scopes = new[] { "User.Read", "User.ReadWrite" };
+            InteractiveBrowserCredentialOptions interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = "INSERT_CLIENT_ID"
+            };
+            var interactiveBrowserCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+            var httpCore = new HttpCore(new AzureIdentityAuthenticationProvider(interactiveBrowserCredential, scopes));
+            var graphClient = new GraphClient(httpCore);
+            var user = await graphClient.Me.GetAsync();
+
+            Console.WriteLine(user.DisplayName);
         }
     }
 }
