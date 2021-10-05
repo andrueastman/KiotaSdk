7e9601e809349d3df91a6f72740429381ac790a4
Andrew Omondi <andrew.omondi@microsoft.com>
2021-10-05 10:28:22 +0300

    Namespace fix

---
+++ Generated/Workbooks/Item/Workbook/Tables/Item/Worksheet/Names/Add/AddRequestBuilder.cs
@@ -0,0 +1,94 @@
+using ApiSdk.Models.Microsoft.Graph;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ApiSdk.Workbooks.Item.Workbook.Tables.Item.Worksheet.Names.Add {
+    /// <summary>Builds and executes requests for operations under \workbooks\{driveItem-id}\workbook\tables\{workbookTable-id}\worksheet\names\microsoft.graph.add</summary>
+    public class AddRequestBuilder {
+        /// <summary>Current path for the request</summary>
+        private string CurrentPath { get; set; }
+        /// <summary>The http core service to use to execute the requests.</summary>
+        private IHttpCore HttpCore { get; set; }
+        /// <summary>Whether the current path is a raw URL</summary>
+        private bool IsRawUrl { get; set; }
+        /// <summary>Path segment to use to build the URL for the current request builder</summary>
+        private string PathSegment { get; set; }
+        /// <summary>
+        /// Instantiates a new AddRequestBuilder and sets the default values.
+        /// <param name="currentPath">Current path for the request</param>
+        /// <param name="httpCore">The http core service to use to execute the requests.</param>
+        /// <param name="isRawUrl">Whether the current path is a raw URL</param>
+        /// </summary>
+        public AddRequestBuilder(string currentPath, IHttpCore httpCore, bool isRawUrl = true) {
+            if(string.IsNullOrEmpty(currentPath)) throw new ArgumentNullException(nameof(currentPath));
+            _ = httpCore ?? throw new ArgumentNullException(nameof(httpCore));
+            PathSegment = "/microsoft.graph.add";
+            HttpCore = httpCore;
+            CurrentPath = currentPath;
+            IsRawUrl = isRawUrl;
+        }
+        /// <summary>
+        /// Invoke action add
+        /// <param name="body"></param>
+        /// <param name="h">Request headers</param>
+        /// <param name="o">Request options for HTTP middlewares</param>
+        /// </summary>
+        public RequestInformation CreatePostRequestInformation(AddRequestBody body, Action<IDictionary<string, string>> h = default, IEnumerable<IMiddlewareOption> o = default) {
+            _ = body ?? throw new ArgumentNullException(nameof(body));
+            var requestInfo = new RequestInformation {
+                HttpMethod = HttpMethod.POST,
+            };
+            requestInfo.SetURI(CurrentPath, PathSegment, IsRawUrl);
+            requestInfo.SetContentFromParsable(HttpCore, "application/json", body);
+            h?.Invoke(requestInfo.Headers);
+            requestInfo.AddMiddlewareOptions(o?.ToArray());
+            return requestInfo;
+        }
+        /// <summary>
+        /// Invoke action add
+        /// <param name="body"></param>
+        /// <param name="h">Request headers</param>
+        /// <param name="o">Request options for HTTP middlewares</param>
+        /// <param name="responseHandler">Response handler to use in place of the default response handling provided by the core service</param>
+        /// </summary>
+        public async Task<AddResponse> PostAsync(AddRequestBody body, Action<IDictionary<string, string>> h = default, IEnumerable<IMiddlewareOption> o = default, IResponseHandler responseHandler = default) {
+            _ = body ?? throw new ArgumentNullException(nameof(body));
+            var requestInfo = CreatePostRequestInformation(body, h, o);
+            return await HttpCore.SendAsync<AddResponse>(requestInfo, responseHandler);
+        }
+        /// <summary>Union type wrapper for classes workbookNamedItem</summary>
+        public class AddResponse : IParsable {
+            /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
+            public IDictionary<string, object> AdditionalData { get; set; }
+            /// <summary>Union type representation for type workbookNamedItem</summary>
+            public WorkbookNamedItem WorkbookNamedItem { get; set; }
+            /// <summary>
+            /// Instantiates a new addResponse and sets the default values.
+            /// </summary>
+            public AddResponse() {
+                AdditionalData = new Dictionary<string, object>();
+            }
+            /// <summary>
+            /// The deserialization information for the current model
+            /// </summary>
+            public IDictionary<string, Action<T, IParseNode>> GetFieldDeserializers<T>() {
+                return new Dictionary<string, Action<T, IParseNode>> {
+                    {"workbookNamedItem", (o,n) => { (o as AddResponse).WorkbookNamedItem = n.GetObjectValue<WorkbookNamedItem>(); } },
+                };
+            }
+            /// <summary>
+            /// Serializes information the current object
+            /// <param name="writer">Serialization writer to use to serialize this model</param>
+            /// </summary>
+            public void Serialize(ISerializationWriter writer) {
+                _ = writer ?? throw new ArgumentNullException(nameof(writer));
+                writer.WriteObjectValue<WorkbookNamedItem>("workbookNamedItem", WorkbookNamedItem);
+                writer.WriteAdditionalData(AdditionalData);
+            }
+        }
+    }
+}
